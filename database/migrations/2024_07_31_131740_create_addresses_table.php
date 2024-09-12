c15e6a9bc1f3b1080be24588e9e676e756c3ac7b
Syed <dcsyedfaraz@gmail.com>
2024-09-12 16:24:40 +0500

    "Added linkedin and logo fields to addresses table and validation rules in ApiController"

--- database/migrations/2024_07_31_131740_create_addresses_table.php
+++ database/migrations/2024_07_31_131740_create_addresses_table.php
@@ -27,6 +27,8 @@ return new class extends Migration {
             // $table->text('personal_notes')->nullable();
             $table->string('feedback')->nullable();
             $table->string('follow_up_date')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('logo')->nullable();
             $table->string('contact_id')->nullable();
             $table->foreignId('sub_project_id')->nullable()->constrained()->onDelete('set null')->onUpdate('cascade');
             // $table->integer('priority')->default(0);
