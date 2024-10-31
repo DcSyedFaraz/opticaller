03eced77c336252d0e593647256d66fc5f8abe74
Syed <dcsyedfaraz@gmail.com>
2024-11-01 00:02:59 +0500

    "Updated call.vue, app.blade.php, and vite.config.js files with changes to imports, configurations, and script additions"

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,19 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <!-- Scripts -->
-    {{-- <script src='https://unpkg.com/webex/umd/calling.min.js'></script> --}}
+    <script src='https://unpkg.com/webex/umd/calling.min.js'></script>
+    <!-- Add inside the <head> tag in your Blade template -->
+    <meta name="webex-access-token" content="{{ env('VITE_WEBEX_ACCESS_TOKEN') }}">
+    <!-- Add inside the <head> tag in your Blade template, before your Vue app scripts -->
+    <script>
+        window.WEBEX_ACCESS_TOKEN = "{{ env('VITE_WEBEX_ACCESS_TOKEN') }}";
+        // console.log(window.WEBEX_ACCESS_TOKEN);
+
+    </script>
+
 
 
     @routes
