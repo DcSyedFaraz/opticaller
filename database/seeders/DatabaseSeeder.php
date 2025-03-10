0958f0b90d8551b64de8c769e9b580f5f2a73507
dcsyedfaraz <dcsyedfaraz@gmail.com>
2025-03-10 17:12:48 +0500

    Add 'milung' role and update related routes and views

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
     {
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'user']);
+        Role::create(['name' => 'milung']);
 
 
         $admin = User::factory()->create([
